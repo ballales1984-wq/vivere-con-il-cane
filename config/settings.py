0346289aec055a94d7b73f3482a2d8d449a6e742
alessio <ballales1984@gmail.com>
2026-04-11 22:18:39 +0200

    Fix DEBUG default and static files config

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", "False").lower() in ("true", "1", "yes")
+DEBUG = os.environ.get("DEBUG", "True").lower() in ("true", "1", "yes")
 
 ALLOWED_HOSTS = ["vivere-con-il-cane.onrender.com", "localhost", "127.0.0.1", "*"]
 
