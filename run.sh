7b07fd65fcd6a17bb1208f721efb8b6a8dbfb936
alessio <ballales1984@gmail.com>
2026-04-13 11:21:37 +0200

    Aggiungi load fixture knowledge in run.sh

--- run.sh
+++ run.sh
@@ -5,5 +5,8 @@ python manage.py migrate --noinput || true
 echo "Loading blog..."
 python manage.py loaddata blog/fixtures/blog_data.json --verbosity 1 || true
 
+echo "Loading knowledge..."
+python manage.py loaddata knowledge/fixtures/knowledge_data.json --verbosity 1 || true
+
 echo "Starting Gunicorn..."
 exec gunicorn config.wsgi:application --bind 0.0.0.0:$PORT
