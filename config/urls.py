d70a695823bfb6cf7d860184bb9c706844e4651d
alessio <ballales1984@gmail.com>
2026-04-13 07:38:42 +0200

    Fix immagini: usa static invece di media per le immagini del blog

--- config/urls.py
+++ config/urls.py
@@ -26,4 +26,4 @@ if settings.DEBUG:
     urlpatterns += static(
         settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0]
     )
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
