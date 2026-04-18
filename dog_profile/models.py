d46e37808adbbd8f14b6cf3978ebf96c67b7ab3e
alessio <ballales1984@gmail.com>
2026-04-18 06:43:07 +0200

    Update: Google Analytics tag position, add owner field to DogProfile

--- dog_profile/models.py
+++ dog_profile/models.py
@@ -1,9 +1,14 @@
 from django.db import models
 from django.utils.text import slugify
+from django.contrib.auth.models import User
 
 
 class DogProfile(models.Model):
     """Owner with their dog profile."""
+
+    owner = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="profiles", null=True, blank=True
+    )
 
     name = models.CharField(max_length=100)
     dog_name = models.CharField(max_length=100)
