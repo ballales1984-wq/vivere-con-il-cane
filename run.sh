28064d5bab974418a8e547b2009fd763f6947d2f
alessio <ballales1984@gmail.com>
2026-05-05 10:49:35 +0200

    fix: load fixture data in run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 python manage.py migrate --noinput
+python manage.py loaddata blog/fixtures/blog_data.json blog/fixtures/initial_data.json knowledge/fixtures/knowledge_data.json || true
 python manage.py collectstatic --noinput
 gunicorn config.wsgi:application --bind 0.0.0.0:$PORT --workers 2 --timeout 120
