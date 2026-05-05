9f5a6d6aac47889dbe1b0410ca0d5a9eb6ba7241
alessio <ballales1984@gmail.com>
2026-05-05 16:51:54 +0200

    perf: add caching to community_stats context processor
    Cache discussions_total and top_contributors for 5 minutes to reduce
    database queries on every request

--- community/context_processors.py
+++ community/context_processors.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import User
+from django.core.cache import cache
 from .models import Discussion, UserReputation, Notification
 
 def community_stats(request):
@@ -7,10 +7,20 @@ def community_stats(request):
     if request.user.is_authenticated:
         unread_count = request.user.community_notifications.filter(is_read=False).count()
 
+    discussions_total = cache.get('community_stats:discussions_total')
+    if discussions_total is None:
+        discussions_total = Discussion.objects.filter(is_approved=True).count()
+        cache.set('community_stats:discussions_total', discussions_total, 300)
+
+    top_contributors = cache.get('community_stats:top_contributors')
+    if top_contributors is None:
+        top_contributors = list(UserReputation.objects.all().order_by("-points")[:5])
+        cache.set('community_stats:top_contributors', top_contributors, 300)
+
     return {
         "community_stats": {
-            "discussions_total": Discussion.objects.filter(is_approved=True).count(),
+            "discussions_total": discussions_total,
         },
-        "top_contributors": UserReputation.objects.all().order_by("-points")[:5],
+        "top_contributors": top_contributors,
         "unread_notifications_count": unread_count,
     }
